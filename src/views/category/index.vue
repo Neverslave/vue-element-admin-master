d25ef84c80791ebcccf239bd96263398c0e6ddfd
Neverslave <1029110566@qq.com>
2019-10-11 22:42:25 +0800

    1111

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,35 +1,39 @@
 <template>
   <div class="category-container">
-    <section>
+    <section class="buttons">
       <el-button type="primary" icon="el-icon-edit">新增</el-button>
       <el-button type="info" icon="el-icon-share">修改</el-button>
       <el-button type="danger" icon="el-icon-delete">删除</el-button>
       <el-button type="primary" icon="el-icon-search">搜索</el-button>
     </section>
+    <hr>
     <div class="el-tree-container">
       <el-tree
-        :props="category"
-        show-checkbox="true"
-        @node-click="handleNodeClick(data)"
-      />
+        :data="category"
+        show-checkbox
+        node-key="id"
+       @check-change="checkCategory"
+        check-strictly
+        ref="categoryTree"
+      >
+      </el-tree>
     </div>
-    <div class="edit-container" />
+    <div class="edit-container"></div>
+
+    <div class="category-input-container">
+
+    </div>
   </div>
+
 </template>
 
 <script>
   import {getCategory} from "@/api/category";
 export default {
   name: 'Index',
-
   data() {
     return {
-      category:{
-        label:'',
-        children:[],
-        disabled:'',
-        isLeaf:''
-   }
+      category:[]
 
     }
   },
@@ -40,17 +44,24 @@ export default {
    return new Promise((resolve,reject) =>{
      getCategory().then(
        response=>{
-         this.category = response
+         this.category = response.data
        }
      )
    } )
 
   },
   methods: {
+    checkCategory(currentNode,ischecked,isLeafChecked){
+   if(ischecked===true){
+     this.$refs.categoryTree.setCheckedKeys([currentNode.id])
+   }else{
+
+   }
+
+    }
 
   },
-  computed(){
-  }
+
 }
 </script>
 
@@ -58,4 +69,8 @@ export default {
 .category-container{
   margin:50px;
 }
+
+  .buttons{
+    margin: 50px;
+  }
 </style>
