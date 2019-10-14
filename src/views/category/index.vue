56d9bb01bed593f23f7846859f26d9e1cd85351e
Neverslave <1029110566@qq.com>
2019-10-14 22:32:20 +0800

    1111

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -23,14 +23,16 @@
     <div class="category-input-container" v-show="isShow">
         <el-form ref="categoryInputForm" label-position="left" label-width="80">
        <el-form-item label="上级菜单"  v-if="isFirst">
-            <el-input v-model="inputCategory.subCategory" size="small">
+            <el-input v-model="inputCategory.subCategory.label" style="width: 30%;"   :disabled="true" size="small">
             </el-input>
        </el-form-item>
           <el-form-item label="分类名称">
-            <el-input type="" v-model="inputCategory.label" style="width: 30%;" size="small"></el-input>
+            <el-input type="" v-model="inputCategory.label" style="width: 30%;"  ></el-input>
           </el-form-item>
+          <div style="width:30% ;margin-left: 10%;" >
           <el-button type="primary" icon="el-icon-check" @click="confirm">确定</el-button>
           <el-button type="danger" icon="el-icon-close"@click="cancel">取消</el-button>
+          </div>
         </el-form>
     </div>
   </div>
@@ -47,7 +49,10 @@ export default {
       inputCategory:{
         id:'',
         label:'',
-        subCategory:{},
+        subCategory:{
+          label:'',
+          id:''
+        },
       },
       isShow:false,
       type:0,
@@ -69,19 +74,22 @@ export default {
 
   },
   methods: {
-    checkCategory(currentNode,ischecked,isLeafChecked){
 
-      var parentNode;
+    checkCategory(currentNode,ischecked,isLeafChecked){
+      let parentNode;
       if (ischecked === true) {
         this.$refs.categoryTree.setCheckedKeys([currentNode.id])
-        parentNode = this.$refs.categoryTree.getNode(currentNode.id).parent.data
+        parentNode = this.$refs.categoryTree.getNode(currentNode.id).parent.data;
+        console.log(parentNode)
+        this.inputCategory.subCategory=parentNode;
       }
 
     },
     addCategory(){
       this.isShow = true
-      //subCategory=this.$refs.categoryTree.getCheckedNodes()[0]
-
+      if(this.inputCategory.subCategory.length ===undefined){
+      this.isFirst = true;
+      }
 
     },
 
@@ -114,5 +122,11 @@ export default {
 
   .buttons{
     margin: 50px;
+  }
+  .category-input-container{
+    margin:0 auto;
+    width:50%;
+    padding: 50px;
+
   }
 </style>
